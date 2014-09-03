64a0fea77275b57c9278f94b32f5ece882345d13
Andrey Shmelev <andrew.shmelev@gmail.com>
2014-09-04 00:58:34 +0400

    ConsoleDemo

---
+++ ConsoleDemo/Actions/PagedListWithView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+using WikiEngine.Dal.Models;
+
+namespace ConsoleDemo.Actions
+{
+    public class PagedListWithView
+    {
+        WikiEngineContext db = new WikiEngineContext();
+        public string GetFiles()
+        {
+            IEnumerable<File> q = db.Set<File>().OrderBy(f => f.Name);
+
+            q = q.ToPagedList(3, 10);
+            
+            return q
+                .Select(f => string.Format("{0}", f.Name))
+                .Aggregate((a, b) => string.Format("{0}{1}{2}", a, Environment.NewLine, b));
+        }
+
+        public string GetFiles2()
+        {
+            IEnumerable<File> q = db.Set<File>().Select(f => f);
+
+            q = q.OrderBy(file => file.Name);
+
+            q = q.ToPagedList(3, 10);
+
+            return q
+                .Select(f => string.Format("{0}", f.Name))
+                .Aggregate((a, b) => string.Format("{0}{1}{2}", a, Environment.NewLine, b));
+        }
+    }
+}
---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleDemo.Actions;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var action = new PagedListWithView();
+            Console.WriteLine(action.GetFiles2());
+            Console.ReadLine();
+        }
+    }
+}
